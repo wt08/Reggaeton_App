8cbb80ef953b3921e1b74c880e86553e428a5ada
Will Tidwell <willtidwell@Wills-Air.hsd1.sc.comcast.net>
2020-09-01 11:41:15 -0400

    refactored API call

--- src/App.js
+++ src/App.js
@@ -8,42 +8,51 @@ import spotifyLogo from "./spotifyLogo";
 import Navbar from "react-bootstrap/Navbar";
 
 function App() {
-
-  const credentials = process.env.REACT_APP_api_credentials
-  const [accessToken, setAccessToken] = useState({})
-  console.log("accessToken ",accessToken)
+  const credentials = process.env.REACT_APP_api_credentials;
+  const [accessToken, setAccessToken] = useState(null);
+  console.log('accesstoken ', accessToken)
   const [artistInfo, setArtistInfo] = useState(null);
-  console.log("artistinfo ", artistInfo)
+  console.log("artistinfo ", artistInfo);
   const [comps, setComps] = useState([]);
   if (comps.length > 2) {
     setComps([]);
   }
 
-  useEffect(() => {
-    const getKey = async () => {
-      const res = await fetch("https://accounts.spotify.com/api/token", {
-        method: "POST",
+  // creating API call function with parameter of access token
+  const makeApiCall = async (token) => {
+    const res = await fetch(
+      "https://api.spotify.com/v1/artists?ids=4q3ewBCX7sLwd24euuV69X,1i8SpTcr7yvPOmcqrbnVXY,4obzFoKoKRHIphyHzJ35G3,7iK8PXO48WeuP03g8YR51W,1GDbiv3spRmZ1XdM1jQbT7",
+      {
         headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-          Authorization:
-            `Basic ${credentials}`,
+          Authorization: `Bearer ${token}`,
         },
-        body: "grant_type=client_credentials",
-      });
-      const json = await res.json();
-      setAccessToken(json.access_token)
-    };
-    const makeApiCall = async () => {
-          const res = await fetch("https://api.spotify.com/v1/artists?ids=4q3ewBCX7sLwd24euuV69X,1i8SpTcr7yvPOmcqrbnVXY,4obzFoKoKRHIphyHzJ35G3,7iK8PXO48WeuP03g8YR51W,1GDbiv3spRmZ1XdM1jQbT7", {
-            headers: {
-              Authorization: `Bearer ${accessToken}`,
-            },
-          });
-          const json = await res.json();
-          setArtistInfo(json.artists);
-        };
-    getKey();
-    makeApiCall();
+      }
+    );
+    const json = await res.json();
+    setArtistInfo(json.artists);
+  };
+
+  // getData function first gets access token, then calls makeApiCall and passes it access token
+  const getData = async () => {
+    // getting access token
+    const res = await fetch("https://accounts.spotify.com/api/token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization: `Basic ${credentials}`,
+      },
+      body: "grant_type=client_credentials",
+    });
+    const json = await res.json();
+    // calling makeApiCall with acquired access token from above
+    makeApiCall(json.access_token);
+    // set access token to variable for use in other comps
+    setAccessToken(json.access_token);
+  };
+
+  // runs getData on every app mount
+  useEffect(() => {
+    getData();
   }, []);
 
   const handleCompToggle = (artist) => {
@@ -89,7 +98,12 @@ function App() {
           <Route
             path="/CompPage"
             render={(routerProps) => (
-              <CompPage {...routerProps} accessToken={accessToken} comps={comps} done={"70"} />
+              <CompPage
+                {...routerProps}
+                accessToken={accessToken}
+                comps={comps}
+                done={"70"}
+              />
             )}
           />
         </Switch>
