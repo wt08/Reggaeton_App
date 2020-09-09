a916c19d480c71d105d8270580c9f91b8402ab2e
Will Tidwell <willtomtid@gmail.com>
2020-09-09 14:48:40 -0400

    deleted select button and comp alert

--- src/components/ArtistCard.js
+++ src/components/ArtistCard.js
@@ -14,10 +14,6 @@ export default function ArtistCard({
     <div className={`artistCard ${comps.includes(artist) ? 'colorEffect' : null}`} onClick={handleCompToggle}>
       <h2>{name}</h2>
       <img className="artistImage" src={imageSrc} alt={name} />
-      {/* <SelectButton handleCompToggle={handleCompToggle} /> */}
-      {/* {comps.length === 2 && comps[comps.length - 1].name === name ? (
-        <CompAlert />
-      ) : null} */}
     </div>
   );
 }
