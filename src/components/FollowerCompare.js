1323fdfbf9d6ade0e3ae9519016880a242cc5bff
Will Tidwell <willtomtid@gmail.com>
2020-09-09 16:50:02 -0400

    basics of FollowerCompare complete

--- src/components/FollowerCompare.js
+++ src/components/FollowerCompare.js
@@ -1,17 +1,24 @@
 import React from "react";
 import Footer from "./Footer";
-import './FollowerCompare.css'
+import "./FollowerCompare.css";
+import CountUp from "react-countup";
 
 const FollowerCompare = ({ comps }) => {
   return (
-    <div>
+    <div className="followerCompare">
+        <h1>Followers:</h1>
       <div className="followersMain">
         {comps.map((artist) => {
           return (
             <div className="followersCard">
-              <h1>{artist.name}</h1>
+              <CountUp
+                end={artist.followers.total}
+                duration={4}
+                separator={","}
+                className="countup"
+              />
+              <h4>{artist.name}</h4>
               <img src={artist.images[0].url} alt={artist.name} />
-              <p>Followers: {artist.followers.total.toLocaleString("en")}</p>
             </div>
           );
         })}
